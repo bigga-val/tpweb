7cd94893fc9d47d58584f6cc234f4d83e161290e
bigga-val <katongeval@esil.ac.cd>
2019-08-24 11:19:09 +0200

    first stape in database with sqlite3

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Sondage(models.Model):
+    question = models.CharField(max_length=200)
+    date_publication = models.DateTimeField()
+
+class Reponse(models.Model):
+    sondage = models.ForeignKey(Sondage,  on_delete=models.CASCADE)
+    choix = models.CharField(max_length=200)
+    nb_votes = models.IntegerField()
