924e210d6f3c23c431042714d9acc6f061659d56
bigga-val <katongeval@esil.ac.cd>
2019-08-25 15:22:36 +0200

    Candidat model creating for linking with the Reponse model to experiment the OneToOne option

--- home/models.py
+++ home/models.py
@@ -1,11 +1,26 @@
 from django.db import models
+from django.utils import timezone #for putting the default time of publishing
+from datetime import datetime
 
 # Create your models here.
 class Sondage(models.Model):
     question = models.CharField(max_length=200)
-    date_publication = models.DateTimeField()
+    date_publication = models.DateTimeField(default= datetime.now)
+
+    def __str__(self):
+        return "{0} {1}".format(self.question, self.date_publication)
 
 class Reponse(models.Model):
     sondage = models.ForeignKey(Sondage,  on_delete=models.CASCADE)
     choix = models.CharField(max_length=200)
-    nb_votes = models.IntegerField()
+    nb_votes = models.CharField(max_length=11, null=True)
+
+    def __str__(self):
+        return "{0} {1} {2}".format(self.choix, self.nb_votes, self.sondage)
+
+class Candidat(models.Model):
+    reponse = models.OneToOneField(Reponse, on_delete=models.SET_NULL, null=True)
+    nom =  models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nom
