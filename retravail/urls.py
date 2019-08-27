c9ed67a6c87c1e4b2cb052979dbe7823ac7a7bee
bigga-val <katongeval@esil.ac.cd>
2019-08-27 09:59:27 +0200

    nada

--- retravail/urls.py
+++ retravail/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from home.views import homepage
+from home import views as v
 
 urlpatterns = [
     path('', views.index),
-    path('sondage/', homepage),
+    path('sondage/', v.homepage),
 ]
